1d65d44ca4bc3a2f53b533f66a29d767d3673110
HsiTingChou <hero70312@gmail.com>
2018-12-29 02:43:50 +0800

    done fix

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const randomInt = require('random-int');
+const bodyParser = require('body-parser');
+const Config = require("./constants/constant");
 const express = require('express');
 
 const app = express();
 
-// // HTTP:413 Request Entity Too Large. => 要確認 express request 預設的最大上限 , 目前設定 100mb
-// app.use(bodyParser.json({limit: '100mb'}));
+// HTTP:413 Request Entity Too Large. => 要確認 express request 預設的最大上限 , 目前設定 100mb
+app.use(bodyParser.json({limit: '100mb'}));
 
 // allow CORS
 app.use(function (req, res, next) {
@@ -20,8 +23,8 @@ app.use('/upload', require('./router/upload'));
 // set public folder as site root
 app.use(express.static('public'));
 
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3007;
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
